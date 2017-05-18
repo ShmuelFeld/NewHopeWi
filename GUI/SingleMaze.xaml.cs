2bb9b2c7e2016a5c25964b38342522c00d0296d3
shlises1 <shani8165@gmail.com>
2017-05-18 15:32:08 +0300

    some shit changed

--- GUI/SingleMaze.xaml.cs
+++ GUI/SingleMaze.xaml.cs
@@ -30,6 +30,7 @@ namespace GUI
             //root.DataContext = this;
             //this.Rows = rows;
             //this.Cols = cols;
+            smVM = new SingleMazeVM();
             this.DataContext = smVM;
             smVM.StartGame();
            // this.MazeString = mazeChars.ToString();
