f73b5d74e7d6de95cf131f694a57cc5a7a7e307a
shlises1 <shani8165@gmail.com>
2017-06-13 16:14:49 +0300

    images2

--- WebApi/View/jquery.MazeJS.js
+++ WebApi/View/jquery.MazeJS.js
@@ -1,26 +1,30 @@
 ﻿(function ($) {
-    $.fn.MazeJS = function (data, player, dest) {
+    $.fn.MazeJS = function (data) {
         maze = data.Maze;
-        var myCanvas = document.getElementById("mazeCanvas");    
+        var myCanvas = document.getElementById("mazeCanvas");  
+        var player = document.getElementById("prince");
+        var dest = document.getElementById("cinderella");
         var context = mazeCanvas.getContext("2d");
         context.clearRect(0, 0, myCanvas.width, myCanvas.height);
-    var rows = data.Rows;
-    var cols = data.Cols;
-    var cellWidth = mazeCanvas.width / cols;
-    var cellHeight = mazeCanvas.height / rows;
-    var counter = 0;
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < cols; j++) {
-            if (maze[counter] == '1') {
-                context.fillRect(cellWidth * j, cellHeight * i,
-                    cellWidth, cellHeight);
+        var rows = data.Rows;
+        var cols = data.Cols;
+        var cellWidth = mazeCanvas.width / cols;
+        var cellHeight = mazeCanvas.height / rows;
+        var counter = 0;
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                if (maze[counter] == '1') {
+                    context.fillRect(cellWidth * j, cellHeight * i,
+                        cellWidth, cellHeight);
+                }
+                counter++;
             }
-            counter++;
         }
-    }
-    context.drawImage(player, data.Start.Col * cellWidth, data.Start.Row * cellHeight,
-        cellWidth, cellHeight);
-    return this;
+        context.drawImage(player, data.Start.Col * cellWidth, data.Start.Row * cellHeight,
+            cellWidth, cellHeight);
+        context.drawImage(dest, data.End.Col * cellWidth, data.End.Row * cellHeight,
+            cellWidth, cellHeight);
+        return this;
 };
 })(jQuery);function move(e) {    if (gameOnBool) {        var keynum;
         if (window.event) { // IE                    
