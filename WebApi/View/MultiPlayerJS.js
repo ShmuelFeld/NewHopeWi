72de6ea785c053cab6f81f28722181343459f4a6
shlises1 <shani8165@gmail.com>
2017-06-19 17:34:03 +0300

    yack

--- WebApi/View/MultiPlayerJS.js
+++ WebApi/View/MultiPlayerJS.js
@@ -1,1 +1,11 @@
-﻿
+﻿var multiplayer = $.connection.MultiPlayerHab;
+
+multiplayer.client.broadcastMessage = function (name, message) {
+    // Add the message to the page
+    alert("msg");
+};
+$.connection.hub.start().done(function () {
+    $("#btnStartGame").click(function () {
+        multiplayer.server.hello();
+    });
+});
